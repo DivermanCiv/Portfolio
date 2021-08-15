f9f3f5c924bcbc9caaddb32a88aa5fba4d592bd6
adam <adamdupuis@laposte.net>
2021-08-15 22:32:36 +0200

    add kelindi blog to project

---
+++ blog_stage.php
@@ -0,0 +1,24 @@
+<?php
+
+include ("admin_config.php");
+
+include("lang_config.php");
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang='fr'>
+    <head>
+        <meta charset='UTF-8' name ="AdamDupuis"/>
+        <title>My Portfolio</title>
+        <link rel='stylesheet' href='styles.css'/>
+    </head>
+    <body>
+      <h1>Expérience pro</h1>
+      <footer>
+          <p>&copy; Adam DUPUIS - 2019</p>
+          <a href="mentionslegales.html"><?= _MENTIONSLEGALES ; ?></a>
+      </footer>
+    </body>
